532819f4a02c0761ac35f7f9b9f0309f636c37cc
Jordan-Castillo <jtcastil@calpoly.edu>
2018-10-12 13:04:04 -0700

    Added a Users class

--- Projects/firstAngularApp/src/app/components/user/user.component.ts
+++ Projects/firstAngularApp/src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component OnInit} from '@angular/core';
+import { User } from '../../models/User';
 //@ts-ignore this is called a decorator, adds extra info to our component
 @Component({
   selector: 'app-user',
@@ -7,29 +8,28 @@ import { Component } from '@angular/core';
   //@ts-ignore template: '<h2>Jordan</h2>'
 })
 
-export class UserComponent {
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-/**example datatypes
-*
-*/
+export class UserComponent implements OnInit{
+  User: User;
+/**
+ * Constructor is used most often
+ * for dependency injection, rather than initializing variables
+ */
   constructor(){
-    this.firstName = 'Jordan';
-    this.lastName = 'Castillo';
-    this.age = 30;
-    this.address = {
-      street: '400 Low st.',
-      city: 'Santa Maria',
-      state: 'CA'
-    };
-  }
-  sayHello(){
-    console.log("Hello " + this.firstName);
 
   }
-  hasBirthday(){
-    this.age += 1;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.User = {
+      firstName: 'Jordan',
+      lastName: 'Castillo',
+      age: 1,
+      address: {
+        street: 'abc st',
+        city: 'sm',
+        state: 'CA'
+      }
+    };
   }
+
 }
--- README.md
+++ README.md
@@ -5,8 +5,27 @@ This repo will include all practice projects written in angular,
 React vs. Angular
     Ang - Core library includes Routing functionality, this must be imported inside React    Ang - Core library includes Routing functionality, this must be imported inside React
 
+NG commands:
+    ng generate component <destination> <flags>
+        ex:
+            ng generate component components\users --spec=false
+        or  ng g c components\users --spec=false
+                                    ^this flag tells ng to not generate the testing files because we will not be using them
+                ^generates the component, and puts the necessary information inside the app.module.ts file
 
+    ng serve <flags>
+        ex:
+            ng serve -o
+                ^serve starts running the scripts of the webpage locally
+                      ^-o then opens the scripts in the local web browser
+
+
+    
 
 For the time being, run these commands before pushing to ensure that correct Git account is uploading:
+
+
 git config user.name Jordan-Castillo
+
+
 git config user.email jtcastil@calpoly.edu
